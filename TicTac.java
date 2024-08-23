9a8fc21e030377d1a6f116f692c4deab74a8aab0
21004764 <143560011+21004764@users.noreply.github.com>
2024-08-23 19:56:05 +0200

    Add files via upload

---
+++ TicTac.java
@@ -0,0 +1,112 @@
+
+package tictactoe;
+
+import java.util.Scanner;
+
+public class TicTac{
+    private final char[][] board;
+    private char currentPlayer;
+
+    public TicTac() {
+        board = new char[3][3];
+        currentPlayer = 'X';
+        initializeBoard();
+    }
+
+    public void play() {
+        printBoard();
+        while (true) {
+            playerMove();
+            printBoard();
+            if (checkForWin()) {
+                System.out.println("Player " + currentPlayer + " wins!");
+                break;
+            }
+            if (isBoardFull()) {
+                System.out.println("The game is a tie!");
+                break;
+            }
+            switchPlayer();
+        }
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    private void printBoard() {
+        System.out.println("Current board:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void playerMove() {
+        Scanner scanner = new Scanner(System.in);
+        int row, col;
+
+        while (true) {
+            System.out.println("Player " + currentPlayer + ", enter your move (row and column): ");
+            row = scanner.nextInt();
+            col = scanner.nextInt();
+
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
+                board[row][col] = currentPlayer;
+                break;
+            } else {
+                System.out.println("This move is not valid.");
+            }
+        }
+    }
+
+    private void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    private boolean checkForWin() {
+        return (checkRows() || checkColumns() || checkDiagonals());
+    }
+
+    private boolean checkRows() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkColumns() {
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonals() {
+        return ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+                (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer));
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+    
+}
